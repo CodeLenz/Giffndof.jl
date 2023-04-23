d554aa23dc5928ab2548e712128dbb537d7a91ed
CodeLenz <eduardobarplenz@gmail.com>
2023-04-23 18:09:28 -0300

    Adding all tests to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,7 @@ include("../examples/common/process.jl")
 
 include("exponential.jl")
 include("dirac.jl")
+include("heaviside0.jl")
+include("heaviside1.jl")
+include("heaviside2.jl")
+include("polynomial.jl")
