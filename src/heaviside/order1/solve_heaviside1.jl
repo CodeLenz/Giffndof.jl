ce895dcd9aec3c75bee8e67cb712c2a65c70ddb3
Eduardo Lenz <eduardobarplenz@gmail.com>
2023-08-15 10:52:38 -0300

    Make sure M is symmetric

--- src/heaviside/order1/solve_heaviside1.jl
+++ src/heaviside/order1/solve_heaviside1.jl
@@ -34,7 +34,7 @@ function Solve_heaviside1(M::AbstractMatrix{T}, C::AbstractMatrix{T},K::Abstract
 
 
     # Evaluate F211 
-    chol = cholesky(M)
+    chol = cholesky(Symmetric(M))
     Kb = chol\K
     Cb = chol\C
     F211 = 0.5*Cb + 0.5*sqrt(Cb^2 - 4*Kb)
