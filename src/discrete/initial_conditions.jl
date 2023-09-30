b1aca755b26d8c73c5f64a1d51cdcb0d6179f980
CodeLenz <eduardobarplenz@gmail.com>
2023-09-30 16:36:40 -0300

    add some . to C1

--- src/discrete/initial_conditions.jl
+++ src/discrete/initial_conditions.jl
@@ -4,11 +4,9 @@
 function calculate_integrationConstants(C_bar, F11, y_p, dy_p, U0, V0)
    
        # Calculates C2
-   
-       C2 = (C_bar-(2*F11))\(V0-dy_p+((F11-C_bar)*(y_p-U0)))
+       C2 = (C_bar.-(2*F11))\(V0.-dy_p.+((F11.-C_bar)*(y_p.-U0)))
    
        # Calculates C1
-   
        C1 = @. U0-y_p-C2
    
        return C1, C2
